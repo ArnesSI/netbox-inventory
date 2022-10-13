b652e56fc8d5445d042f604ff1dd568b8807b5ba
Kristijan Šuler <kristijan.suler@gmail.com>
2022-10-13 15:03:48 +0200

    Fix: add default values

--- netbox_inventory/__init__.py
+++ netbox_inventory/__init__.py
@@ -19,6 +19,9 @@ class NetBoxInventoryConfig(PluginConfig):
         'asset_import_create_device_type': False,
         'asset_import_create_module_type': False,
         'asset_import_create_inventoryitem_type': False,
+        'asset_disable_editing_fields_for_tags': {},
+        'asset_disable_deletion_for_tags': [],
     }
+
 
 config = NetBoxInventoryConfig
