ae6d3e481cfa266fa1902069d1d58c880598061f
Matej Vadnjal <matej.vadnjal@arnes.si>
2022-10-23 17:35:07 +0200

    add asset info panel  on device, module & inventory-item details

--- netbox_inventory/template_content.py
+++ netbox_inventory/template_content.py
@@ -1,14 +1,32 @@
 from extras.plugins import PluginTemplateExtension
 from .models import Asset
 
-class DeviceAssetInfo(PluginTemplateExtension):
-    model = 'dcim.device'
-
+class AssetInfoExtension(PluginTemplateExtension):
     def left_page(self):
-        device = self.context.get('object')
-        asset = Asset.objects.filter(device=device).first()
+        object = self.context.get('object')
+        asset = Asset.objects.filter(**{self.kind:object}).first()
         return self.render('netbox_inventory/inc/asset_info.html', extra_context={
             'asset': asset,
         })
 
-template_extensions = [DeviceAssetInfo]
+
+class DeviceAssetInfo(AssetInfoExtension):
+    model = 'dcim.device'
+    kind = 'device'
+
+
+class ModuleAssetInfo(AssetInfoExtension):
+    model = 'dcim.module'
+    kind = 'module'
+
+
+class InventoryItemAssetInfo(AssetInfoExtension):
+    model = 'dcim.inventoryitem'
+    kind = 'inventoryitem'
+
+
+template_extensions = (
+    DeviceAssetInfo,
+    ModuleAssetInfo,
+    InventoryItemAssetInfo,
+)
