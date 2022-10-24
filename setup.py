56bab50a5885a5cd48b348c01bb8b85abde1d81e
Matej Vadnjal <matej.vadnjal@arnes.si>
2022-10-24 15:28:43 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox-inventory',
-    version='0.9.0',
+    version='1.0.0',
     description='Inventory asset management in NetBox',
     install_requires=[],
     packages=find_packages(),
