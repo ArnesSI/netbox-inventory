53d3f313ef4712895efdeaf8df388251ae5b3b6c
Matej Vadnjal <matej.vadnjal@arnes.si>
2022-12-09 13:18:02 +0100

    bump version

--- netbox_inventory/tests/test_load.py
+++ netbox_inventory/tests/test_load.py
@@ -11,7 +11,7 @@ class NetboxDnsVersionTestCase(SimpleTestCase):
     """
 
     def test_version(self):
-        assert __version__ == "1.0.4"
+        assert __version__ == "1.0.5"
 
 
 class AppTest(APITestCase):
