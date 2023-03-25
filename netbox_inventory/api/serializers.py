06a3377f17a9d1b1911e1f8801ee86e8e624a1a4
Matej Vadnjal <matej.vadnjal@arnes.si>
2023-03-25 17:40:58 +0100

    Fix API for Assets
    don't consider model's unique_together in serializer's validation

--- netbox_inventory/api/serializers.py
+++ netbox_inventory/api/serializers.py
@@ -3,34 +3,47 @@ from rest_framework import serializers
 from dcim.api.serializers import (
     NestedDeviceTypeSerializer, NestedDeviceSerializer,
     NestedManufacturerSerializer,
-    NestedModuleTypeSerializer, NestedModuleSerializer
+    NestedModuleTypeSerializer, NestedModuleSerializer,
+    NestedInventoryItemSerializer
 )
+from tenancy.api.serializers import NestedContactSerializer, NestedTenantSerializer
 from netbox.api.serializers import NetBoxModelSerializer
 from .nested_serializers import *
 from ..models import Asset, InventoryItemType, InventoryItemGroup, Purchase, Supplier
-
 
 
 class AssetSerializer(NetBoxModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='plugins-api:netbox_inventory-api:asset-detail'
     )
-    device_type = NestedDeviceTypeSerializer()
-    device = NestedDeviceSerializer()
-    module_type = NestedModuleTypeSerializer()
-    module = NestedModuleSerializer()
-    storage_location = NestedModuleSerializer()
-    purchase = NestedPurchaseSerializer()
+    device_type = NestedDeviceTypeSerializer(required=False)
+    device = NestedDeviceSerializer(required=False)
+    module_type = NestedModuleTypeSerializer(required=False)
+    module = NestedModuleSerializer(required=False)
+    inventoryitem_type = NestedInventoryItemTypeSerializer(required=False) 
+    inventoryitem = NestedInventoryItemSerializer(required=False)
+    storage_location = NestedModuleSerializer(required=False)
+    purchase = NestedPurchaseSerializer(required=False)
+    tenant = NestedTenantSerializer(required=False)
+    contact = NestedContactSerializer(required=False)
+    owner = NestedTenantSerializer(required=False)
+
 
     class Meta:
         model = Asset
         fields = (
             'id', 'url', 'display', 'name', 'asset_tag', 'serial', 'status',
-            'kind', 'device_type', 'device', 'module_type', 'module',
+            'kind', 'device_type', 'device', 'module_type', 'module', 'inventoryitem_type','inventoryitem', 
             'tenant', 'contact', 'storage_location', 'owner', 'purchase',
             'warranty_start', 'warranty_end',
             'comments', 'tags', 'custom_fields', 'created', 'last_updated'
         )
+        # DRF autiomatically creates validator from model's unique_together contraints
+        # that doesn't work if we allow some filelds in a unique_together to be null
+        # so we remove DRF's auto generated validators and rely on model's validation
+        # logic to handle validation
+        # see  https://www.django-rest-framework.org/api-guide/validators/#optional-fields
+        validators = []
 
 
 class SupplierSerializer(NetBoxModelSerializer):
