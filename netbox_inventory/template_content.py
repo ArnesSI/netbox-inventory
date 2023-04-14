f4be290b1b18eb56b24035fbd9453b9f6c3ad92b
Matej Vadnjal <matej.vadnjal@arnes.si>
2023-04-14 12:47:01 +0200

    show count of related assets on contact, manufacturer, device_type and module_type

--- netbox_inventory/template_content.py
+++ netbox_inventory/template_content.py
@@ -14,6 +14,22 @@ class AssetInfoExtension(PluginTemplateExtension):
         return self.render('netbox_inventory/inc/asset_info.html', extra_context=context)
 
 
+class AssetTypeStats(PluginTemplateExtension):
+    def right_page(self):
+        object = self.context.get('object')
+        user = self.context['request'].user
+        context = {
+            'asset_stats': [
+                {
+                    'label': 'Total',
+                    'filter_field': f'{self.kind}_id',
+                    'count': Asset.objects.restrict(user, 'view').filter(**{self.kind:object}).count(),
+                },
+            ],
+        }
+        return self.render('netbox_inventory/inc/asset_stats_counts.html', extra_context=context)
+
+
 class DeviceAssetInfo(AssetInfoExtension):
     model = 'dcim.device'
     kind = 'device'
@@ -29,6 +45,54 @@ class InventoryItemAssetInfo(AssetInfoExtension):
     kind = 'inventoryitem'
 
 
+class DeviceTypeAssetInfo(AssetTypeStats):
+    model = 'dcim.devicetype'
+    kind = 'device_type'
+
+
+class ModuleTypeAssetInfo(AssetTypeStats):
+    model = 'dcim.moduletype'
+    kind = 'module_type'
+
+
+class ManufacturerAssetInfo(PluginTemplateExtension):
+    model = 'dcim.manufacturer'
+    def right_page(self):
+        object = self.context.get('object')
+        user = self.context['request'].user
+        count_device = Asset.objects.restrict(user, 'view').filter(device_type__manufacturer=object).count()
+        count_module = Asset.objects.restrict(user, 'view').filter(module_type__manufacturer=object).count()
+        count_inventoryitem = Asset.objects.restrict(user, 'view').filter(inventoryitem_type__manufacturer=object).count()
+        context = {
+            'asset_stats': [
+                {
+                    'label': 'Device',
+                    'filter_field': 'manufacturer_id',
+                    'extra_filter': '&kind=device',
+                    'count': count_device,
+                },
+                {
+                    'label': 'Module',
+                    'filter_field': 'manufacturer_id',
+                    'extra_filter': '&kind= module',
+                    'count': count_module,
+                },
+                {
+                    'label': 'Inventory Item',
+                    'filter_field': 'manufacturer_id',
+                    'extra_filter': '&kind=inventoryitem',
+                    'count': count_inventoryitem,
+                },
+                {
+                    'label': 'Total',
+                    'filter_field': 'manufacturer_id',
+                    'count': count_device + count_module + count_inventoryitem,
+                },
+            ],
+        }
+        return self.render('netbox_inventory/inc/asset_stats_counts.html', extra_context=context)
+
+
 class TenantAssetInfo(PluginTemplateExtension):
     model = 'tenancy.tenant'
     def right_page(self):
@@ -41,9 +105,30 @@ class TenantAssetInfo(PluginTemplateExtension):
         return self.render('netbox_inventory/inc/asset_tenant.html', extra_context=context)
 
 
+class ContactAssetInfo(PluginTemplateExtension):
+    model = 'tenancy.contact'
+    def right_page(self):
+        object = self.context.get('object')
+        user = self.context['request'].user
+        context = {
+            'asset_stats': [
+                {
+                    'label': 'Assigned',
+                    'filter_field': 'contact_id',
+                    'count': Asset.objects.restrict(user, 'view').filter(contact=object).count(),
+                },
+            ],
+        }
+        return self.render('netbox_inventory/inc/asset_stats_counts.html', extra_context=context)
+
+
 template_extensions = (
     DeviceAssetInfo,
     ModuleAssetInfo,
     InventoryItemAssetInfo,
+    DeviceTypeAssetInfo,
+    ModuleTypeAssetInfo,
+    ManufacturerAssetInfo,
     TenantAssetInfo,
+    ContactAssetInfo,
 )
