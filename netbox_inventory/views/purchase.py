160561a4c6686b467d4db8832bdef4f033e304e3
Matej Vadnjal <matej.vadnjal@arnes.si>
2023-09-13 09:39:33 +0200

    replace delivery netsted table with htmx in purchase detail view

--- netbox_inventory/views/purchase.py
+++ netbox_inventory/views/purchase.py
@@ -16,19 +16,9 @@ class PurchaseView(generic.ObjectView):
     queryset = models.Purchase.objects.all()
 
     def get_extra_context(self, request, instance):
-        purchase_deliveries = models.Delivery.objects.restrict(request.user, 'view').filter(
-            purchase=instance
-        )
         purchase_assets = models.Asset.objects.restrict(request.user, 'view').filter(
             purchase=instance
         )
-
-        delivery_table = tables.DeliveryTable(purchase_deliveries, user=request.user)
-        delivery_table.columns.hide('purchase')
-        delivery_table.columns.hide('purchase_date')
-        delivery_table.columns.hide('supplier')
-        delivery_table.configure(request)
-
         asset_table = tables.AssetTable(purchase_assets, user=request.user)
         asset_table.columns.hide('purchase')
         asset_table.columns.hide('purchase_date')
@@ -38,7 +28,6 @@ class PurchaseView(generic.ObjectView):
         return {
             'asset_table': asset_table,
             'asset_count': models.Asset.objects.filter(purchase=instance).count(),
-            'delivery_table': delivery_table,
             'delivery_count': models.Delivery.objects.filter(purchase=instance).count(),
         }
 
