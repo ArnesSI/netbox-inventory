6f2060cfc1e9800da2044882e9b05e4517568350
Matej Vadnjal <matej.vadnjal@arnes.si>
2024-05-07 15:03:46 +0200

    Extend serializer for brief mode
    and remove nested serializers were possible

--- netbox_inventory/api/serializers.py
+++ netbox_inventory/api/serializers.py
@@ -1,33 +1,119 @@
 from rest_framework import serializers
 
 from dcim.api.serializers import (
-    NestedDeviceTypeSerializer, NestedDeviceSerializer,
-    NestedManufacturerSerializer,
-    NestedModuleTypeSerializer, NestedModuleSerializer,
-    NestedInventoryItemSerializer, NestedLocationSerializer
+    DeviceTypeSerializer, DeviceSerializer, ManufacturerSerializer,
+    ModuleTypeSerializer, ModuleSerializer, InventoryItemSerializer,
+    LocationSerializer
 )
-from tenancy.api.serializers import NestedContactSerializer, NestedTenantSerializer
-from netbox.api.serializers import NetBoxModelSerializer
+from tenancy.api.serializers import ContactSerializer, TenantSerializer
+from netbox.api.serializers import NestedGroupModelSerializer, NetBoxModelSerializer
 from .nested_serializers import *
 from ..models import Asset, Delivery, InventoryItemType, InventoryItemGroup, Purchase, Supplier
+
+
+class SupplierSerializer(NetBoxModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_inventory-api:supplier-detail'
+    )
+    asset_count = serializers.IntegerField(read_only=True)
+    purchase_count = serializers.IntegerField(read_only=True)
+    delivery_count = serializers.IntegerField(read_only=True)
+    
+    class Meta:
+        model = Supplier
+        fields = (
+            'id', 'url', 'display', 'name', 'slug', 'description', 'comments',
+            'tags', 'custom_fields', 'created', 'last_updated', 'asset_count',
+            'purchase_count', 'delivery_count',
+        )
+        brief_fields = ('id', 'url', 'display', 'name', 'slug')
+
+
+class PurchaseSerializer(NetBoxModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_inventory-api:purchase-detail'
+    )
+    supplier = SupplierSerializer(nested=True)
+    asset_count = serializers.IntegerField(read_only=True)
+    delivery_count = serializers.IntegerField(read_only=True)
+    
+    class Meta:
+        model = Purchase
+        fields = (
+            'id', 'url', 'display', 'supplier', 'name', 'status', 'date', 'description',
+            'comments', 'tags', 'custom_fields', 'created', 'last_updated',
+            'asset_count', 'delivery_count',
+        )
+        brief_fields = ('id', 'url', 'display', 'supplier', 'name', 'status', 'date')
+
+
+class DeliverySerializer(NetBoxModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_inventory-api:delivery-detail'
+    )
+    purchase = PurchaseSerializer(nested=True)
+    receiving_contact = ContactSerializer(nested=True, required=False, allow_null=True, default=None)
+    asset_count = serializers.IntegerField(read_only=True)
+    
+    class Meta:
+        model = Delivery
+        fields = (
+            'id', 'url', 'display', 'purchase', 'name', 'date', 'description', 'comments',
+            'receiving_contact', 'tags', 'custom_fields', 'created', 'last_updated', 'asset_count',
+        )
+        brief_fields = ('id', 'url', 'display', 'name', 'date')
+
+
+class InventoryItemGroupSerializer(NestedGroupModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_inventory-api:inventoryitemgroup-detail'
+    )
+    parent = NestedInventoryItemGroupSerializer(required=False, allow_null=True, default=None)
+    asset_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = InventoryItemGroup
+        fields = (
+            'id', 'url', 'display', 'name', 'parent', 'comments', 'tags', 'custom_fields',
+            'created', 'last_updated', 'asset_count', '_depth',
+        )
+        brief_fields = ('id', 'url', 'display', 'name', '_depth')
+
+
+class InventoryItemTypeSerializer(NetBoxModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_inventory-api:inventoryitemtype-detail'
+    )
+    manufacturer = ManufacturerSerializer(nested=True)
+    inventoryitem_group = InventoryItemGroupSerializer(nested=True, required=False, allow_null=True, default=None)
+    asset_count = serializers.IntegerField(read_only=True)
+    
+    class Meta:
+        model = InventoryItemType
+        fields = (
+            'id', 'url', 'display', 'model', 'slug', 'manufacturer', 'part_number',
+            'inventoryitem_group', 'comments', 'tags', 'custom_fields', 'created',
+            'last_updated', 'asset_count',
+        )
+        brief_fields = ('id', 'url', 'display', 'manufacturer', 'model', 'slug')
 
 
 class AssetSerializer(NetBoxModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='plugins-api:netbox_inventory-api:asset-detail'
     )
-    device_type = NestedDeviceTypeSerializer(required=False, allow_null=True, default=None)
-    device = NestedDeviceSerializer(required=False, allow_null=True, default=None)
-    module_type = NestedModuleTypeSerializer(required=False, allow_null=True, default=None)
-    module = NestedModuleSerializer(required=False, allow_null=True, default=None)
-    inventoryitem_type = NestedInventoryItemTypeSerializer(required=False, allow_null=True, default=None) 
-    inventoryitem = NestedInventoryItemSerializer(required=False, allow_null=True, default=None)
-    storage_location = NestedLocationSerializer(required=False, allow_null=True, default=None)
-    delivery = NestedDeliverySerializer(required=False, allow_null=True, default=None)
-    purchase = NestedPurchaseSerializer(required=False, allow_null=True, default=None)
-    tenant = NestedTenantSerializer(required=False, allow_null=True, default=None)
-    contact = NestedContactSerializer(required=False, allow_null=True, default=None)
-    owner = NestedTenantSerializer(required=False, allow_null=True, default=None)
+    device_type = DeviceTypeSerializer(nested=True, required=False, allow_null=True, default=None)
+    device = DeviceSerializer(nested=True, required=False, allow_null=True, default=None)
+    module_type = ModuleTypeSerializer(nested=True, required=False, allow_null=True, default=None)
+    module = ModuleSerializer(nested=True, required=False, allow_null=True, default=None)
+    inventoryitem_type = InventoryItemTypeSerializer(nested=True, required=False, allow_null=True, default=None) 
+    inventoryitem = InventoryItemSerializer(nested=True, required=False, allow_null=True, default=None)
+    storage_location = LocationSerializer(nested=True, required=False, allow_null=True, default=None)
+    delivery = DeliverySerializer(nested=True, required=False, allow_null=True, default=None)
+    purchase = PurchaseSerializer(nested=True, required=False, allow_null=True, default=None)
+    tenant = TenantSerializer(nested=True, required=False, allow_null=True, default=None)
+    contact = ContactSerializer(nested=True, required=False, allow_null=True, default=None)
+    owner = TenantSerializer(nested=True, required=False, allow_null=True, default=None)
 
     def to_internal_value(self, data):
         ret = super().to_internal_value(data)
@@ -49,6 +135,7 @@ class AssetSerializer(NetBoxModelSerializer):
             'warranty_start', 'warranty_end',
             'comments', 'tags', 'custom_fields', 'created', 'last_updated'
         )
+        brief_fields = ('id', 'url', 'display', 'serial')
         # DRF autiomatically creates validator from model's unique_together contraints
         # that doesn't work if we allow some filelds in a unique_together to be null
         # so we remove DRF's auto generated validators and rely on model's validation
@@ -56,84 +143,3 @@ class AssetSerializer(NetBoxModelSerializer):
         # see  https://www.django-rest-framework.org/api-guide/validators/#optional-fields
         validators = []
 
-
-class SupplierSerializer(NetBoxModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name='plugins-api:netbox_inventory-api:supplier-detail'
-    )
-    asset_count = serializers.IntegerField(read_only=True)
-    purchase_count = serializers.IntegerField(read_only=True)
-    delivery_count = serializers.IntegerField(read_only=True)
-    
-    class Meta:
-        model = Supplier
-        fields = (
-            'id', 'url', 'display', 'name', 'slug', 'description', 'comments',
-            'tags', 'custom_fields', 'created', 'last_updated', 'asset_count',
-            'purchase_count', 'delivery_count',
-        )
-
-
-class PurchaseSerializer(NetBoxModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name='plugins-api:netbox_inventory-api:purchase-detail'
-    )
-    supplier = NestedSupplierSerializer()
-    asset_count = serializers.IntegerField(read_only=True)
-    delivery_count = serializers.IntegerField(read_only=True)
-    
-    class Meta:
-        model = Purchase
-        fields = (
-            'id', 'url', 'display', 'supplier', 'name', 'status', 'date', 'description',
-            'comments', 'tags', 'custom_fields', 'created', 'last_updated',
-            'asset_count', 'delivery_count',
-        )
-
-
-class DeliverySerializer(NetBoxModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name='plugins-api:netbox_inventory-api:delivery-detail'
-    )
-    purchase = NestedPurchaseSerializer()
-    receiving_contact = NestedContactSerializer(required=False, allow_null=True, default=None)
-    asset_count = serializers.IntegerField(read_only=True)
-    
-    class Meta:
-        model = Delivery
-        fields = (
-            'id', 'url', 'display', 'purchase', 'name', 'date', 'description', 'comments',
-            'receiving_contact', 'tags', 'custom_fields', 'created', 'last_updated', 'asset_count',
-        )
-
-
-class InventoryItemTypeSerializer(NetBoxModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name='plugins-api:netbox_inventory-api:inventoryitemtype-detail'
-    )
-    manufacturer = NestedManufacturerSerializer()
-    inventoryitem_group = NestedInventoryItemGroupSerializer(required=False, allow_null=True, default=None)
-    asset_count = serializers.IntegerField(read_only=True)
-    
-    class Meta:
-        model = InventoryItemType
-        fields = (
-            'id', 'url', 'display', 'model', 'slug', 'manufacturer', 'part_number',
-            'inventoryitem_group', 'comments', 'tags', 'custom_fields', 'created',
-            'last_updated', 'asset_count',
-        )
-
-
-class InventoryItemGroupSerializer(NetBoxModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name='plugins-api:netbox_inventory-api:inventoryitemgroup-detail'
-    )
-    parent = NestedInventoryItemGroupSerializer(required=False, allow_null=True, default=None)
-    asset_count = serializers.IntegerField(read_only=True)
-    
-    class Meta:
-        model = InventoryItemGroup
-        fields = (
-            'id', 'url', 'display', 'name', 'parent', 'comments', 'tags', 'custom_fields',
-            'created', 'last_updated', 'asset_count',
-        )
