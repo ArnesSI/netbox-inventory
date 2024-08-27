6bac64227da91906bc4c7d2a81326583ce9873d1
Joshua Coronado <jcoronado@coreweave.com>
2024-08-27 13:45:56 -0700

    feat: update graphql for netbox v4

--- netbox_inventory/graphql.py
+++ netbox_inventory/graphql.py
@@ -1,14 +1,7 @@
-from graphene import ObjectType
-from netbox.graphql.types import NetBoxObjectType
-from netbox.graphql.fields import ObjectField, ObjectListField
-from .filtersets import (
-    AssetFilterSet,
-    SupplierFilterSet,
-    PurchaseFilterSet,
-    DeliveryFilterSet,
-    InventoryItemTypeFilterSet,
-    InventoryItemGroupFilterSet,
-)
+import strawberry
+import strawberry_django
+from typing import Annotated
+
 from .models import (
     Asset,
     Supplier,
@@ -17,64 +10,87 @@ from .models import (
     InventoryItemType,
     InventoryItemGroup,
 )
+from .filtersets import (
+    AssetFilterSet,
+    SupplierFilterSet,
+    PurchaseFilterSet,
+    DeliveryFilterSet,
+    InventoryItemTypeFilterSet,
+    InventoryItemGroupFilterSet,
+)
+from netbox.graphql.filter_mixins import autotype_decorator, BaseFilterMixin
 
+# Filter definitions using existing FilterSets
+@strawberry_django.filter(Asset)
+@autotype_decorator(AssetFilterSet)
+class AssetFilter(BaseFilterMixin):
+    pass
 
-class AssetType(NetBoxObjectType):
-    class Meta:
-        model = Asset
-        fields = "__all__"
-        filterset_class = AssetFilterSet
+@strawberry_django.filter(Supplier)
+@autotype_decorator(SupplierFilterSet)
+class SupplierFilter(BaseFilterMixin):
+    pass
 
+@strawberry_django.filter(Purchase)
+@autotype_decorator(PurchaseFilterSet)
+class PurchaseFilter(BaseFilterMixin):
+    pass
 
-class SupplierType(NetBoxObjectType):
-    class Meta:
-        model = Supplier
-        fields = "__all__"
-        filterset_class = SupplierFilterSet
+@strawberry_django.filter(Delivery)
+@autotype_decorator(DeliveryFilterSet)
+class DeliveryFilter(BaseFilterMixin):
+    pass
 
+@strawberry_django.filter(InventoryItemType)
+@autotype_decorator(InventoryItemTypeFilterSet)
+class InventoryItemTypeFilter(BaseFilterMixin):
+    pass
 
-class PurchaseType(NetBoxObjectType):
-    class Meta:
-        model = Purchase
-        fields = "__all__"
-        filterset_class = PurchaseFilterSet
+@strawberry_django.filter(InventoryItemGroup)
+@autotype_decorator(InventoryItemGroupFilterSet)
+class InventoryItemGroupFilter(BaseFilterMixin):
+    pass
 
+# Type definitions
+@strawberry_django.type(Asset, fields="__all__", filters=AssetFilter)
+class AssetType:
+    pass
 
-class DeliveryType(NetBoxObjectType):
-    class Meta:
-        model = Delivery
-        fields = "__all__"
-        filterset_class = DeliveryFilterSet
+@strawberry_django.type(Supplier, fields="__all__", filters=SupplierFilter)
+class SupplierType:
+    pass
 
+@strawberry_django.type(Purchase, fields="__all__", filters=PurchaseFilter)
+class PurchaseType:
+    pass
 
-class InventoryItemTypeType(NetBoxObjectType):
-    class Meta:
-        model = InventoryItemType
-        fields = "__all__"
-        filterset_class = InventoryItemTypeFilterSet
+@strawberry_django.type(Delivery, fields="__all__", filters=DeliveryFilter)
+class DeliveryType:
+    pass
 
+@strawberry_django.type(InventoryItemType, fields="__all__", filters=InventoryItemTypeFilter)
+class InventoryItemTypeType:
+    pass
 
-class InventoryItemGroupType(NetBoxObjectType):
-    class Meta:
-        model = InventoryItemGroup
-        fields = "__all__"
-        filterset_class = InventoryItemGroupFilterSet
+@strawberry_django.type(InventoryItemGroup, fields="__all__", filters=InventoryItemGroupFilter)
+class InventoryItemGroupType:
+    pass
 
+# Query definition
+@strawberry.type
+class Query:
+    asset: AssetType = strawberry_django.field()
+    supplier: SupplierType = strawberry_django.field()
+    purchase: PurchaseType = strawberry_django.field()
+    delivery: DeliveryType = strawberry_django.field()
+    inventory_item_type: InventoryItemTypeType = strawberry_django.field()
+    inventory_item_group: InventoryItemGroupType = strawberry_django.field()
 
-class Query(ObjectType):
-    asset = ObjectField(AssetType)
-    supplier = ObjectField(SupplierType)
-    purchase = ObjectField(PurchaseType)
-    delivery = ObjectField(DeliveryType)
-    inventory_item_type = ObjectField(InventoryItemTypeType)
-    inventory_item_group = ObjectField(InventoryItemGroupType)
+    asset_list: list[AssetType] = strawberry_django.field()
+    supplier_list: list[SupplierType] = strawberry_django.field()
+    purchase_list: list[PurchaseType] = strawberry_django.field()
+    delivery_list: list[DeliveryType] = strawberry_django.field()
+    inventory_item_type_list: list[InventoryItemTypeType] = strawberry_django.field()
+    inventory_item_group_list: list[InventoryItemGroupType] = strawberry_django.field()
 
-    asset_list = ObjectListField(AssetType)
-    supplier_list = ObjectListField(SupplierType)
-    purchase_list = ObjectListField(PurchaseType)
-    delivery_list = ObjectListField(DeliveryType)
-    inventory_item_type_list = ObjectListField(InventoryItemTypeType)
-    inventory_item_group_list = ObjectListField(InventoryItemGroupType)
-
-
-schema = Query
+schema = strawberry.Schema(query=Query)
