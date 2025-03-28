f899178af3d47642f42105489d4cc39a05684b01
Lachlan Tripolone <lachlan@sol1.com.au>
2025-03-28 12:17:25 +1100

    Added dedicated template for bulk scan page

--- netbox_inventory/views/asset.py
+++ netbox_inventory/views/asset.py
@@ -218,6 +218,7 @@ class AssetBulkScanView(generic.BulkEditView):
     filterset = filtersets.AssetFilterSet
     table = tables.AssetTable
     form = forms.AssetBulkScanForm
+    template_name = 'netbox_inventory/asset_bulk_scan.html'
 
     def post(self, request, **kwargs):
         """Override post method to check if assets are protected from editing"""
@@ -238,54 +239,56 @@ class AssetBulkScanView(generic.BulkEditView):
 
         errors = []
         protected_assets = []
+        if '_apply' not in request.POST:
+            return super().post(request, **kwargs)
 
-        if '_apply' in request.POST:
-            form = self.form(request.POST, initial=initial_data)
-            restrict_form_fields(form, request.user)
+        form = self.form(request.POST, initial=initial_data)
+        restrict_form_fields(form, request.user)
+        if not form.is_valid():
+            return super().post(request, **kwargs)
 
-            if form.is_valid():
-                nullified_fields = set(request.POST.getlist('_nullify'))
+        nullified_fields = set(request.POST.getlist('_nullify'))
 
-                queryset = self.queryset.filter(pk__in=pk_list)
+        queryset = self.queryset.filter(pk__in=pk_list)
 
-                for asset in queryset:
-                    asset_tags = set(asset.tags.all().values_list('slug', flat=True))
-                    intersection_of_tags = set(asset_tags).intersection(
-                        protected_fields_by_tags.keys()
+        for asset in queryset:
+            asset_tags = set(asset.tags.all().values_list('slug', flat=True))
+            intersection_of_tags = set(asset_tags).intersection(
+                protected_fields_by_tags.keys()
+            )
+
+            # Check if asset is protected from editing
+            for tag in intersection_of_tags:
+                protected_fields = set(protected_fields_by_tags[tag])
+
+                modified_fields = set(form.changed_data)
+                nullable = set(form.nullable_fields).intersection(
+                    set(nullified_fields)
+                )
+
+                if modified_fields.intersection(
+                    protected_fields
+                ) or nullable.intersection(protected_fields):
+                    break
+
+                protected_assets.append(asset)
+
+                fields = modified_fields.intersection(
+                    protected_fields
+                ).union(nullable.intersection(protected_fields))
+                errors.append(
+                    'Cannot edit asset {} fields protected by tag {}: {}.'.format(
+                        asset,
+                        tag,
+                        ','.join(fields),
                     )
-
-                    # Check if asset is protected from editing
-                    for tag in intersection_of_tags:
-                        # TODO: Check if custom fields can be protected
-                        protected_fields = set(protected_fields_by_tags[tag])
-
-                        modified_fields = set(form.changed_data)
-                        nullable = set(form.nullable_fields).intersection(
-                            set(nullified_fields)
-                        )
-
-                        if modified_fields.intersection(
-                            protected_fields
-                        ) or nullable.intersection(protected_fields):
-                            protected_assets.append(asset)
-
-                            fields = modified_fields.intersection(
-                                protected_fields
-                            ).union(nullable.intersection(protected_fields))
-                            errors.append(
-                                'Cannot edit asset {} fields protected by tag {}: {}.'.format(
-                                    asset,
-                                    tag,
-                                    ','.join(fields),
-                                )
-                            )
-                if errors:
-                    error_msg_protected_assets = f'Edit failed for all assets. Because of trying to modify protected fields on assets: {", ".join(map(str, set(protected_assets)))}.'
-                    logger.info(errors + [error_msg_protected_assets])
-                    messages.warning(request, ' '.join(errors))
-                    messages.warning(request, error_msg_protected_assets)
-                    return redirect(self.get_return_url(request))
-        return super().post(request, **kwargs)
+                )
+        if errors:
+            error_msg_protected_assets = f'Edit failed for all assets. Because of trying to modify protected fields on assets: {", ".join(map(str, set(protected_assets)))}.'
+            logger.info(errors + [error_msg_protected_assets])
+            messages.warning(request, ' '.join(errors))
+            messages.warning(request, error_msg_protected_assets)
+            return redirect(self.get_return_url(request))
 
 
 @register_model_view(models.Asset, 'bulk_delete', path='delete', detail=False)
