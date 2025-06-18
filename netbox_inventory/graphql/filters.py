c0db938b588daea8c2571dec7b8a2a9dd9c3b169
Matej Vadnjal <matej.vadnjal@arnes.si>
2025-06-18 21:57:46 +0200

    graphql: remove autotype_decorator

--- netbox_inventory/graphql/filters.py
+++ netbox_inventory/graphql/filters.py
@@ -1,6 +1,5 @@
 import strawberry_django
-
-from netbox.graphql.filter_mixins import BaseFilterMixin, autotype_decorator
+from netbox.graphql.filter_mixins import BaseFilterMixin
 
 from netbox_inventory import filtersets, models
 
@@ -15,36 +14,30 @@ __all__ = (
 
 
 @strawberry_django.filter(models.Asset, lookups=True)
-@autotype_decorator(filtersets.AssetFilterSet)
 class AssetFilter(BaseFilterMixin):
     pass
 
 
 @strawberry_django.filter(models.Supplier, lookups=True)
-@autotype_decorator(filtersets.SupplierFilterSet)
 class SupplierFilter(BaseFilterMixin):
     pass
 
 
 @strawberry_django.filter(models.Purchase, lookups=True)
-@autotype_decorator(filtersets.PurchaseFilterSet)
 class PurchaseFilter(BaseFilterMixin):
     pass
 
 
 @strawberry_django.filter(models.Delivery, lookups=True)
-@autotype_decorator(filtersets.DeliveryFilterSet)
 class DeliveryFilter(BaseFilterMixin):
     pass
 
 
 @strawberry_django.filter(models.InventoryItemType, lookups=True)
-@autotype_decorator(filtersets.InventoryItemTypeFilterSet)
 class InventoryItemTypeFilter(BaseFilterMixin):
     pass
 
 
 @strawberry_django.filter(models.InventoryItemGroup, lookups=True)
-@autotype_decorator(filtersets.InventoryItemGroupFilterSet)
 class InventoryItemGroupFilter(BaseFilterMixin):
     pass
