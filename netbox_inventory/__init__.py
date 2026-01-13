4b078051e518e833408fa5044e9555ef05ac6566
Matej Vadnjal <matej.vadnjal@arnes.si>
2026-01-13 14:55:19 +0100

    bump netbox min_version

--- netbox_inventory/__init__.py
+++ netbox_inventory/__init__.py
@@ -13,7 +13,7 @@ class NetBoxInventoryConfig(PluginConfig):
     author = 'Matej Vadnjal'
     author_email = 'matej.vadnjal@arnes.si'
     base_url = 'inventory'
-    min_version = '4.4.0'
+    min_version = '4.5.0'
     default_settings = {
         'top_level_menu': True,
         'used_status_name': 'used',
