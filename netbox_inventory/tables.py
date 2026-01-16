e33d4cede213da88f2260996f1d4e9c62576354e
Matej Vadnjal <matej.vadnjal@arnes.si>
2026-01-16 18:11:20 +0100

    rename asset column to asset count

--- netbox_inventory/tables.py
+++ netbox_inventory/tables.py
@@ -720,7 +720,7 @@ class AuditTrailTable(NetBoxTable):
 asset_count = columns.LinkedCountColumn(
     viewname='plugins:netbox_inventory:asset_list',
     url_params={'device_type_id': 'pk'},
-    verbose_name=_('Assets'),
+    verbose_name=_('Asset Count'),
     accessor='assets__count',
     orderable=False,
 )
@@ -731,7 +731,7 @@ register_table_column(asset_count, 'assets', DeviceTypeTable)
 asset_count = columns.LinkedCountColumn(
     viewname='plugins:netbox_inventory:asset_list',
     url_params={'module_type_id': 'pk'},
-    verbose_name=_('Assets'),
+    verbose_name=_('AsseAsset Countts'),
     accessor='assets__count',
     orderable=False,
 )
@@ -742,7 +742,7 @@ register_table_column(asset_count, 'assets', ModuleTypeTable)
 asset_count = columns.LinkedCountColumn(
     viewname='plugins:netbox_inventory:asset_list',
     url_params={'rack_type_id': 'pk'},
-    verbose_name=_('Assets'),
+    verbose_name=_('Asset Count'),
     accessor='assets__count',
     orderable=False,
 )
@@ -753,7 +753,7 @@ register_table_column(asset_count, 'assets', RackTypeTable)
 asset_count = columns.LinkedCountColumn(
     viewname='plugins:netbox_inventory:asset_list',
     url_params={'storage_location_id': 'pk'},
-    verbose_name=_('Assets'),
+    verbose_name=_('Asset Count'),
     # accessor='assets__count',
     accessor=tables.A('assets__count_with_children'),
     orderable=False,
